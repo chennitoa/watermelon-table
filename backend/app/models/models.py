dbce00db3a9b830b10f4f06481efae7493559315
Austin Chen <achennito@gmail.com>
2024-05-04 15:46:39 -0700

    Updated models for consistency

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -57,11 +57,10 @@ class UpdateListing(BaseModel):
     listing_id: int
     title: Optional[str]
     description: Optional[str]
-    address: Optional[str]
+    location: Optional[str]
 
 
 class SearchListings(BaseModel):
-    listing_id: Optional[int]
     username: Optional[str]
     title: Optional[str]
     description: Optional[str]
