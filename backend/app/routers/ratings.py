51a4e2945db71af79f35037e0227cf9a3f2f084c
stanley-nguyen <stanley.nguyen@sjsu.edu>
2024-05-04 23:54:39 -0700

    Add ratings

---
+++ backend/app/routers/ratings.py
@@ -0,0 +1,83 @@
+from fastapi import APIRouter, HTTPException
+
+from ..models import models
+from ..services.db import rating_manager
+
+
+router = APIRouter(
+    prefix='/ratings',
+    tags=['ratings']
+)
+
+
+@router.post("/")
+def rate_profile(rating: models.Rating):
+    '''Submits a rating for a profile'''
+    if not 1 <= rating.rating <= 5:
+        raise HTTPException(status_code=400, detail="Invalid rating. Rating must be between 1-5.")
+
+    status = rating_manager.rate_profile(rating.rater_name, rating.rated_name, rating.rating)
+
+    if status:
+        return {
+            "message": f"User {rating.rater_name} has successfully rated User {rating.rated_name} a {rating.rating} out of 5.",
+            "status": "success"
+        }
+    else:
+        raise HTTPException(status_code=409, detail=f"Failed to create rating for user {rating.rated_name}.")
+
+
+@router.put("/update/")
+def update_rating(update: models.Rating):
+    """Update an existing rating."""
+    status = rating_manager.update_rating(update.rater_name, update.rated_name, update.rating)
+
+    if status:
+        return {
+            "message": f"Rating for user {update.rated_name} has been updated.",
+            "status": "success"
+        }
+    else:
+        raise HTTPException(status_code=409, detail=f"Failed to update rating for user {update.rated_name}")
+
+
+@router.get("/all/{username}")
+def get_rating(username: str, is_rater: bool):
+    """Get all existing ratings for a user."""
+    ratings = rating_manager.get_rating(username)
+
+    if ratings:
+        return {
+            "result": ratings,
+            "status": "success"
+        }
+    else:
+        raise HTTPException(status_code=404, detail="Failed to find user.")
+
+
+@router.get("/{rater_name}/{rated_name}")
+def get_specific_rating(rater_name: str, rated_name: str):
+    """Get all existing ratings for a user."""
+    ratings = rating_manager.get_specific_rating(rater_name, rated_name)
+
+    if ratings:
+        return {
+            "result": ratings,
+            "status": "success"
+        }
+    else:
+        raise HTTPException(status_code=404, detail="Failed to find user.")
+
+
+@router.get("/{username}")
+def get_avg_rating(username: str):
+    """Get the average rating for a user."""
+    rating = rating_manager.get_avg_rating(username)
+
+    if rating:
+        return {
+            "result": rating,
+            "status": "success"
+        }
+    else:
+        raise HTTPException(status_code=404, detail="Failed to find user.")
---
+++ backend/app/services/db/rating_manager.py
@@ -0,0 +1,187 @@
+from datetime import datetime
+
+from .db_connect import connect
+from .user_manager import get_user
+
+
+def rate_profile(rater_name: str, rated_name: str, rating: int):
+    """Rates an existing profile.
+
+    Args:
+        rater_name (str): The username of the rating user.
+        rated_name (str): The username of the user receiving a rating.
+        rating (int): The rating received by the receiving user. Ranges from 1 to 5.
+
+    Returns:
+        True if the function succeeds, else False.
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor()
+
+        # Ensure users cannot rate themselves
+        if rater_name == rated_name:
+            return False
+
+        # Check if users exist
+        userA_details = get_user(rater_name, is_username=True)
+        userB_details = get_user(rated_name, is_username=True)
+
+        if not userA_details or not userB_details:
+            return False
+
+        # Check if a rating already exists
+        cursor.execute(f'''
+                       SELECT * FROM ratings
+                       WHERE rater_name = \"{rater_name}\" AND rated_name = \"{rated_name}\"
+                       ''')
+
+        # If a rating already exists, update the rating.
+        if cursor.fetchall():
+            update_rating(rater_name, rated_name, rating)
+
+            return True
+
+        query = '''
+        INSERT INTO ratings (rater_name, rated_name, rating, rated_date)
+        VALUES (%s, %s, %s, %s)
+        '''
+
+        values = (
+            userA_details['username'],
+            userB_details['username'],
+            rating,
+            datetime.now()
+        )
+
+        # Insert ratings into ratings table
+        cursor.execute(query, values)
+        conn.commit()
+
+    return True
+
+
+def update_rating(rater_name: str, rated_name: str, rating: int):
+    """Updates the information of a user in the database.
+
+    Args:
+        rater_name (str): The username of the rating user.
+        rated_name (str): The username of the user receiving a rating.
+        rating (int): The rating received by the receiving user. Ranges from 1 to 5.
+
+    Returns:
+        True if the function succeeds, else False.
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor()
+
+        # Check if users exist
+        existing = get_specific_rating(rater_name, rated_name)
+
+        if not existing:
+            return False
+
+        query = '''
+        UPDATE ratings SET
+        rating = COALESCE(%s, rating)
+        WHERE rater_name = %s AND rated_name = %s
+        '''
+
+        values = (
+            rating,
+            rater_name,
+            rated_name
+        )
+
+        cursor.execute(query, values)
+        conn.commit()
+
+    return True
+
+
+def get_rating(username: str, is_rater: bool = True):
+    """Searches for the ratings of a user in the database. If found, returns all ratings.
+
+    Args:
+        username (str): The username associated with the user.
+        is_rater (bool): If true, returns all outgoing ratings from the user. Else, returns all ratings received.
+
+    Returns:
+        A list of all ratings received or given by the user.
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor(dictionary=True)
+
+        # Check if user exists and get the user id
+        user_details = get_user(username, is_username=True)
+
+        if not user_details:
+            return None
+
+        if is_rater:
+            query = "SELECT * FROM ratings WHERE rater_name = %s"
+        else:
+            query = "SELECT * FROM ratings WHERE rated_name = %s"
+
+        cursor.execute(query, (user_details['username'], ))
+        ratings = cursor.fetchall()
+
+    return ratings
+
+
+def get_specific_rating(rater_name: str, rated_name: str):
+    """Searches for a specific rating in the database.
+
+    Args:
+        rater_name (str): The username of the rating user.
+        rated_name (str): The username of the user receiving a rating.
+
+    Returns:
+        A dict with the details of the specific rating.
+    """
+
+    with connect() as conn:
+        cursor = conn.cursor(dictionary=True)
+
+        # Check if users exist
+        userA_details = get_user(rater_name, is_username=True)
+        userB_details = get_user(rated_name, is_username=True)
+
+        if not userA_details or not userB_details:
+            return None
+
+        query = "SELECT * FROM ratings WHERE rater_name = %s AND rated_name = %s"
+
+        cursor.execute(query, (rater_name, rated_name,))
+        ratings = cursor.fetchone()
+
+    return ratings
+
+
+def get_avg_rating(username: str):
+    """Searches for the ratings of a user in the database and returns the average.
+
+    Args:
+        username (str): The username associated with the user.
+
+    Returns:
+        A float representing the average ratings received by the user.
+    """
+
+    ratings = get_rating(username, is_rater=False)
+
+    if not ratings:
+        return None
+
+    num = 0
+    den = 0
+
+    for user in ratings:
+        num += user['rating']
+        den += 1
+
+    avg_ratings = num / den
+
+    return avg_ratings
