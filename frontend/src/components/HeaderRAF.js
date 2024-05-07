0e6dff18cc2f4ab09470097ed4993167457b6212
Andrew Vu <andrewkvu2@gmail.com>
2024-05-06 18:59:47 -0700

    added some buttons that work

--- frontend/src/components/HeaderRAF.js
+++ frontend/src/components/HeaderRAF.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import MessageIcon from "@mui/icons-material/Message";
+import { Link } from "react-router-dom";
+import { getCurrentUser, getProfile } from "../client";
+
 function HeaderRAF() {
+  const [currentUsername, setUsername] = useState('');
+  const [profilePicture, setProfilePicture] = useState('')
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await getCurrentUser(); 
+        setUsername(user.username);
+        const data = await getProfile(user.username);
+        setProfilePicture(data['result']['profile_picture']);
+      } catch (error) {
+        console.error('Failed to fetch profile:', error);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
+  console.log()
+
+  const handleSignOut = () => {
+    // Clear access token from localStorage
+    console.log(localStorage.getItem('token'));
+    localStorage.removeItem('token');
+    const accessToken = localStorage.getItem('token');
+    console.log(accessToken);
+    // You may want to redirect the user to the sign-in page or perform other actions after signing out
+  };
+
   return (
-    <Box p={2}>
+    <Box
+      p={2}
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      sx={{ backgroundColor: '#B3BFB8' }} // Background color set to light shade of orange
+    >
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Button component={Link} to="/dms" variant="contained" startIcon={<MessageIcon />}>My DMs</Button>
+        <Button component={Link} to="/listings" variant="contained" >Listings</Button>
+      </Box>
       <Typography
         variant="h1"
         sx={{
@@ -11,12 +56,15 @@ function HeaderRAF() {
           flexDirection: "column",
           alignItems: "center",
           textAlign: "center",
+          justifyContent: "center", // Center horizontally
+          height: "100%", // Take up full height of parent container
         }}
       >
         <Typography
           component="h1"
           variant="h1"
           sx={{
+            paddingLeft: "70px",
             display: "flex",
             flexDirection: { xs: "column", md: "row" },
             alignSelf: "center",
@@ -29,6 +77,11 @@ function HeaderRAF() {
           </Typography>
         </Typography>
       </Typography>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Button component={Link} to={`/profile/${currentUsername}`} variant="contained" startIcon={<Avatar src={profilePicture}/>}>{currentUsername}</Button>
+        <Button onClick={handleSignOut} component={Link} to="/sign-in" variant="contained" >Sign Out</Button> 
+        {/* delete the session/access token */}
+      </Box>
     </Box>
   );
 }
