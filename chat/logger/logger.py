eeb4799239f420bbcc1d8ab50d39897b784f51ed
Austin Chen <achennito@gmail.com>
2024-05-13 18:10:08 -0700

    Delete useless %f

--- chat/logger/logger.py
+++ chat/logger/logger.py
@@ -17,7 +17,7 @@ class Logger:
         handler.setFormatter(
             colorlog.ColoredFormatter(
                 "%(log_color)s%(asctime)s [%(name)s] %(levelname)s %(message)s",
-                datefmt="%Y-%m-%d %H:%M:%S:%f",
+                datefmt="%Y-%m-%d %H:%M:%S",
                 log_colors={
                     "DEBUG": "cyan",
                     "INFO": "green",
